855b5b7db320471289d15f4e1a91b039a91a930a
Mike Chaliy <mike@chaliy.name>
2010-08-15 21:59:08 +0300

    Added remote bus

---
+++ src/SampleCommands/CommandRegistry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleCommands
+{
+    public static class CommandRegistry
+    {
+        public static Type GetCommand(string key)
+        {
+            return Type.GetType("Inventory.Domain.Commands." + key + ", Inventory.Domain");
+        }
+    }
+}
---
+++ src/SampleRemoteApp/CommandBus.svc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CommandRouter;
+using CommandRouter.Remote;
+using SampleCommands;
+using SampleHandlers;
+
+namespace SampleRemoteApp
+{
+    public class SampleCommandBusService : Server.CommandBusService
+    {
+        public override ICommandBus CreateCommandBus()
+        {
+            return new LocalCommandBus(CommandHandlerResolvers.FromAssembly(typeof(SampleHandlersMarker).Assembly));            
+        }
+
+        public override Type ResolveCommand(string key)
+        {
+            return CommandRegistry.GetCommand(key);
+        }
+    }
+}
