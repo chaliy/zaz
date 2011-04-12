819b7671eb6dd311126b44bff5c4ed86d92f6fbe
Mike Chaliy <mike@chaliy.name>
2011-04-12 23:35:09 +0300

    Sample powershell script improved

--- src/SampleAppScript.ps1
+++ src/SampleAppScript.ps1
@@ -1,6 +1,10 @@
-import-module PsGet # Requires PsGet installed
-
-install-module https://github.com/chaliy/psurl/raw/master/PsUrl/PsUrl.psm1
+if (-not (get-module PsUrl -ListAvailable)){
+	if (-not (get-module PsGet -ListAvailable)){
+		throw "Script requires PsGet (https://github.com/chaliy/psget) installed"
+	}
+	import-module PsGet	
+	install-module https://github.com/chaliy/psurl/raw/master/PsUrl/PsUrl.psm1
+}
 import-module PsUrl
 
 Write-Url "http://localhost:9302/Commands/" -Data @{ "Zaz-Command-Id" = "SampleCommands.PrintMessage";
