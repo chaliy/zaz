e8cddb6e96479589a3f2407caabe7e3bfc3d65ec
Mike Chaliy <mike@chaliy.name>
2011-06-15 15:50:53 +0300

    Sample PowerShell client fixed

--- src/SampleAppScript.ps1
+++ src/SampleAppScript.ps1
@@ -1,11 +1,5 @@
-if (-not (get-module PsUrl -ListAvailable)){
-	if (-not (get-module PsGet -ListAvailable)){
-		throw "Script requires PsGet (https://github.com/chaliy/psget) installed"
-	}
-	import-module PsGet	
-	install-module https://github.com/chaliy/psurl/raw/master/PsUrl/PsUrl.psm1
-}
-import-module PsUrl
+import-module PsGet	# From http://psget.net
+install-module PsUrl
 
 Write-Url "http://localhost:9302/Commands/" -Data @{ "Zaz-Command-Id" = "SampleCommands.PrintMessage";
 													 "Message" = "Hello world!" }
