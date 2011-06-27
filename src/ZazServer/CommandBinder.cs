d04002c12aee6073accf9d62df7db5a29c496c9a
Mike Chaliy <mike@chaliy.name>
2011-06-27 23:58:40 +0300

    Command binder now supports everything from old Zaz client

--- src/ZazServer/CommandBinder.cs
+++ src/ZazServer/CommandBinder.cs
@@ -7,18 +7,41 @@ namespace Zaz.Server
     {
         public object Build(Type cmdType, IDictionary<string, string> data)
         {
-            var cmd = Activator.CreateInstance(cmdType);
+            var cmd = Activator.CreateInstance(cmdType);            
 
-            foreach (var key in data.Keys)
+            foreach(var prop in cmdType.GetProperties())
             {
-                var prop = cmdType.GetProperty(key);
-                if (prop != null)
+                var val = NormalizeValue(prop.PropertyType, data[prop.Name]);
+                prop.SetValue(cmd, val, null);
+            }            
+           
+            return cmd;
+        }
+
+        private object NormalizeValue(Type t, string v){
+            if (t == typeof(Guid))
+            {
+                return new System.Guid(v);                
+            }
+            else if (t.IsEnum)
+            {
+                return Enum.Parse(t, v);
+            }
+            else if (t == typeof(Boolean))
+            {
+                return Boolean.Parse(v);
+            }
+            else if (t.IsGenericType && t.GetGenericTypeDefinition() == typeof(Nullable<>))
+                return NormalizeValue(Nullable.GetUnderlyingType(t), v);
+            else
+            {
+                var converter = System.ComponentModel.TypeDescriptor.GetConverter(t);
+                if (converter != null && converter.CanConvertFrom(typeof(string)))
                 {
-                    prop.SetValue(cmd, data[key], null);
+                    return converter.ConvertFromString(v);
                 }
             }
-
-            return cmd;
+            return v;
         }
     }
 }
