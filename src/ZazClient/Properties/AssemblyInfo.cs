6788335e5db1150f93925841eef799621ae08630
Mike Chaliy <mike@chaliy.name>
2011-07-16 01:26:53 +0300

    New version

--- src/ZazClient/Properties/AssemblyInfo.cs
+++ src/ZazClient/Properties/AssemblyInfo.cs
@@ -2,10 +2,10 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("ZazClient")]
-[assembly: AssemblyDescription("Client for Zaz Command Bus. Super simple REST Command Bus. Uses WCF Web API as main transport.")]
+[assembly: AssemblyDescription("[WARNING: NOT PRODUCTION READY] Client for Zaz Command Bus. Super simple REST Command Bus. Uses WCF Web API as main transport.")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("f7c063cc-f6e6-46c9-be48-d60704681420")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
--- src/ZazServer/Properties/AssemblyInfo.cs
+++ src/ZazServer/Properties/AssemblyInfo.cs
@@ -2,10 +2,10 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("ZazServer")]
-[assembly: AssemblyDescription("Zaz Command Bus Server. Super simple REST Command Bus. Uses WCF Web API as main transport.")]
+[assembly: AssemblyDescription("[WARNING: NOT PRODUCTION READY] Zaz Command Bus Server. Super simple REST Command Bus. Uses WCF Web API as main transport.")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("3f87fdf1-b651-4fb0-99a1-ec762d607eef")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
